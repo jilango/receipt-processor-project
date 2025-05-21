da0881a5e83469149426507626121ad9ec6a9b49
Jack Praveen Raj Ilango <142956239+jilango@users.noreply.github.com>
2025-05-21 17:25:04 +0000

    feat(api): implement GET /receipts/{id}/points to return temporary stored points

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Receipt
 from uuid import uuid4
 import storage_db
@@ -16,3 +16,11 @@ def process_receipt(receipt: Receipt):
     receipt_id = str(uuid4())
     storage_db.db[receipt_id] = {"points": 1111}  # Test Value
     return {"id": receipt_id}
+
+
+@app.get("/receipts/{id}/points")
+def get_points(id: str):
+    if id not in storage_db.db:
+        raise HTTPException(
+            status_code=404, detail="No receipt found for that ID.")
+    return {"points": storage_db.db[id]["points"]}
