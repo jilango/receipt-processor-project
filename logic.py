bc75e38b8e76c997e5c17d1e375138d0d4a85173
Jack Praveen Raj Ilango <142956239+jilango@users.noreply.github.com>
2025-05-21 18:48:43 +0000

    feat(logic): add rules for divisible by 0.25, item pair bonus and item description bonus

--- logic.py
+++ logic.py
@@ -1,7 +1,9 @@
 from models import Receipt
+import math
 
 
 def calculate_points(receipt: Receipt) -> int:
+
     points = 0
 
     # Rule 1: One point for every alphanumeric character in the retailer name.
@@ -14,5 +16,23 @@ def calculate_points(receipt: Receipt) -> int:
             points += 50
     except ValueError:
         pass
+
+    # Rule 3: 25 points if the total is a multiple of 0.25.
+    if (total * 100) % 25 == 0:
+        points += 25
+
+    # Rule 4: 5 points for every two items on the receipt.
+    points += (len(receipt.items) // 2) * 5
+
+    # Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+    for item in receipt.items:
+        desc = item.shortDescription.strip()
+        if len(desc) % 3 == 0:
+            try:
+                price = float(item.price)
+                bonus = math.ceil(price * 0.2)
+                points += bonus
+            except ValueError:
+                continue
 
     return points
